63ee1c59104a28270e4d1267342b956c142e6bcc
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2025-02-13 00:26:05 +0900

    add: 画面の表示を実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,19 @@
-'use client';
-import SelfLocationMarker from '@/components/SelfLocationMarker';
-import { logger } from '@/utils/logger';
-import { Map } from '@vis.gl/react-google-maps';
-import { useRef, useState } from 'react';
+import SwipeableDrawer from "@/components/SwipeableDrawer";
+import Navigation from "@/components/Navigation";
+import { LocationTrackingMap } from "@/components/maps";
+import { BottomControl } from "@/components/BottomControl";
 
-const Home = () => {
-  const currentLocation = useRef<{lat: number, lng: number}>(undefined);
-  const isFirstRender = useRef(true);
-  const [mapCenter, setMapCenter] = useState<{lat: number, lng: number}>({ lat: 35.6813, lng: 139.767066 });
-
-  const getCurrentPosition = () => {
-    if (!isFirstRender.current) return;
-    isFirstRender.current = false;
-
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        currentLocation.current = { 
-          lat: position.coords.latitude, 
-          lng: position.coords.longitude 
-        };
-        setMapCenter(currentLocation.current);
-        logger('info', `Current location: ${currentLocation.current.lat}, ${currentLocation.current.lng}`, {
-          calledBy: 'Home', 
-          statusCode: 200
-        });
-      }, 
-      (error) => {
-        logger('error', error.message, {
-          calledBy: 'Home', 
-          statusCode: 500
-        });
-      }
+const Page = () => {
+    return (
+        <>
+            <div className="absolute top-5 right-5 left-5 flex items-center justify-end z-10">
+                <Navigation />
+            </div>
+            <LocationTrackingMap />
+            <BottomControl />
+            <SwipeableDrawer />
+        </>
     );
-  };
-
-  return (
-    <Map
-      defaultCenter={mapCenter}
-      defaultZoom={16}
-      minZoom={10}
-      maxZoom={20}
-      gestureHandling={'greedy'}
-      disableDefaultUI={true}
-      mapId={'main-map'}
-      onIdle={getCurrentPosition}
-      className='w-full h-full'>
-        <SelfLocationMarker position={currentLocation.current}/>
-    </Map>
-  );
 };
 
-export default Home;
+export default Page;
