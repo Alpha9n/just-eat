51d9446acd638aa83ef84795bf6abf10153f6caa
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2025-02-13 00:12:36 +0900

    doc: jsdocを追加

--- src/utils/hotpepper.ts
+++ src/utils/hotpepper.ts
@@ -1,5 +1,10 @@
 export const RANGES = [300, 500, 1000, 2000, 3000] as const;
 
+/**
+ * hotPepperAPI 用の距離を人間にわかりやすい距離単位に変換する
+ * @param range 
+ * @returns 距離（メートル）
+ */
 export const exchangeRangeToMeter = (range: 1 | 2 | 3 | 4 | 5): number => {
     return RANGES[range - 1];
 };
