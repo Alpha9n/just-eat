443920ea1733774c6ddba00ea7fb9584785c5b71
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2025-02-13 00:11:29 +0900

    add: ロードコンポーネントの追加

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,12 @@
-import { CircularProgress } from "@mui/material";
+import { Loader } from "lucide-react";
 
 const Loading = () => {
     return (
-        <CircularProgress />
+        <div className="flex flex-col gap-5 w-full h-full justify-center items-center px-0 py-5">
+            <p className="text-xl font-bold">Loading</p>
+            <Loader className="animate-spin size-10 text-amber-600" />
+        </div>
     );
-}
+};
 
 export default Loading;
