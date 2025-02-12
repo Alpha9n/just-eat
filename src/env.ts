2acda40089e932ea66c165c3f802a439e1d38b5c
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2025-02-13 00:03:50 +0900

    add: envの構成ファイルを追加

---
+++ src/env.ts
@@ -0,0 +1,17 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+    server: {
+        HOT_PEPPER_API_URL: z.string(),
+        HOT_PEPPER_API_KEY: z.string(),
+    },
+    client: {
+        NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: z.string(),
+    },
+    runtimeEnv: {
+        HOT_PEPPER_API_URL: process.env.HOT_PEPPER_API_URL,
+        HOT_PEPPER_API_KEY: process.env.HOT_PEPPER_API_KEY,
+        NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    },
+});
